75140c80bfffe8e4f2f5842346dd20f4a0649d41
muhammadrosyidd <rosyidmuhammad612@gmail.com>
2025-04-19 19:39:47 +0700

    landing-page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,379 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <link rel="apple-touch-icon" sizes="76x76" href="assets/img//apple-icon.png">
+  <link rel="icon" type="image/png" href="assets/img//favicon.png">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <title>
+    Paper Kit by Creative Tim
+  </title>
+  <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
+  <!--     Fonts and icons     -->
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
+  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+  <!-- CSS Files -->
+  <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
+  <link href="assets/css/paper-kit.css?v=2.2.0" rel="stylesheet" />
+  <!-- CSS Just for demo purpose, don't include it in your project -->
+  <link href="assets/demo/demo.css" rel="stylesheet" />
+</head>
+
+<body id="top" class="landing-page sidebar-collapse">
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg fixed-top navbar-transparent " color-on-scroll="300">
+    <div class="container">
+      <div class="navbar-translate">
+        <a class="navbar-brand" rel="tooltip" title="POLINEMA" data-placement="bottom" target="_blank">
+          <img src="../public/assets/img/polinema_logo.png" alt="Logo" style="height: 30px; margin-right: 10px;">
+          D4 SISTEM INFORMASI BISNIS
+        </a>
+        <button class="navbar-toggler navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-bar bar1"></span>
+          <span class="navbar-toggler-bar bar2"></span>
+          <span class="navbar-toggler-bar bar3"></span>
+        </button>
+      </div>
+      <div class="collapse navbar-collapse justify-content-end" id="navigation">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a href="#" class="nav-link"><i class="fa fa-home"></i> Beranda</a>
+          </li>          
+          <li class="nav-item dropdown">
+            <a href="#" class="nav-link dropdown-toggle" id="navbarDropdownKriteria" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              <i class="nc-icon nc-book-bookmark"></i> Kriteria
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdownKriteria">
+              <a class="dropdown-item" href="#">Kriteria 1</a>
+              <a class="dropdown-item" href="#">Kriteria 2</a>
+              <a class="dropdown-item" href="#">Kriteria 4</a>
+              <a class="dropdown-item" href="#">Kriteria 5</a>
+              <a class="dropdown-item" href="#">Kriteria 6</a>
+              <a class="dropdown-item" href="#">Kriteria 7</a>
+              <a class="dropdown-item" href="#">Kriteria 8</a>
+              <a class="dropdown-item" href="#">Kriteria 9</a>
+              
+            </div>
+          </li>
+          
+          {{-- <li class="nav-item">
+            <a class="nav-link" rel="tooltip" title="Follow us on Twitter" data-placement="bottom" href="https://twitter.com/CreativeTim" target="_blank">
+              <i class="fa fa-twitter"></i>
+              <p class="d-lg-none">Twitter</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" rel="tooltip" title="Like us on Facebook" data-placement="bottom" href="https://www.facebook.com/CreativeTim" target="_blank">
+              <i class="fa fa-facebook-square"></i>
+              <p class="d-lg-none">Facebook</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" rel="tooltip" title="Follow us on Instagram" data-placement="bottom" href="https://www.instagram.com/CreativeTimOfficial" target="_blank">
+              <i class="fa fa-instagram"></i>
+              <p class="d-lg-none">Instagram</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" rel="tooltip" title="Star on GitHub" data-placement="bottom" href="https://www.github.com/CreativeTimOfficial" target="_blank">
+              <i class="fa fa-github"></i>
+              <p class="d-lg-none">GitHub</p>
+            </a>
+          </li> --}}
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <!-- End Navbar -->
+  <div class="page-header" data-parallax="true" style="position: relative; overflow: hidden; height: 100vh;">
+    <video autoplay muted loop playsinline
+           style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; object-fit: cover; z-index: -1;">
+      <source src="assets/img/jti.mp4" type="video/mp4">
+    </video>
+    <div style="background-color: rgba(0, 123, 255, 0.4); position: absolute; top: 0; left: 0; width: 100%; height: 100%; z-index: -1;"></div>
+    <div class="filter"></div>
+    <div class="container">
+      <div class="motto text-center">
+        <h1 style="font-weight: 900">SELAMAT DATANG DI</h1>
+        <h3 style="font-weight: 700; font-size: 30px;">SISTEM AKREDITASI JTI POLINEMA</h3>
+        <br />
+        <button type="button" class="btn btn-primary btn-round">LOGIN</button>
+      </div>
+    </div>
+  </div>
+  <div class="main">
+    {{-- <div class="section text-center">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 ml-auto mr-auto">
+            <h2 class="title">Let's talk product</h2>
+            <h5 class="description">This is the paragraph where you can write more details about your product. Keep you user engaged by providing meaningful information. Remember that by this time, the user is curious, otherwise he wouldn't scroll to get here. Add a button if you want the user to see more.</h5>
+            <br>
+            <a href="#paper-kit" class="btn btn-danger btn-round">See Details</a>
+          </div>
+        </div>
+        <br/>
+        <br/> --}}
+        {{-- <div class="row">
+          <div class="col-md-3">
+            <div class="info">
+              <div class="icon icon-danger">
+                <i class="nc-icon nc-album-2"></i>
+              </div>
+              <div class="description">
+                <h4 class="info-title">Beautiful Gallery</h4>
+                <p class="description">Spend your time generating new ideas. You don't have to think of implementing.</p>
+                <a href="javascript:;" class="btn btn-link btn-danger">See more</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="info">
+              <div class="icon icon-danger">
+                <i class="nc-icon nc-bulb-63"></i>
+              </div>
+              <div class="description">
+                <h4 class="info-title">New Ideas</h4>
+                <p>Larger, yet dramatically thinner. More powerful, but remarkably power efficient.</p>
+                <a href="javascript:;" class="btn btn-link btn-danger">See more</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="info">
+              <div class="icon icon-danger">
+                <i class="nc-icon nc-chart-bar-32"></i>
+              </div>
+              <div class="description">
+                <h4 class="info-title">Statistics</h4>
+                <p>Choose from a veriety of many colors resembling sugar paper pastels.</p>
+                <a href="javascript:;" class="btn btn-link btn-danger">See more</a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-3">
+            <div class="info">
+              <div class="icon icon-danger">
+                <i class="nc-icon nc-sun-fog-29"></i>
+              </div>
+              <div class="description">
+                <h4 class="info-title">Delightful design</h4>
+                <p>Find unique and handmade delightful designs related items directly from our sellers.</p>
+                <a href="javascript:;" class="btn btn-link btn-danger">See more</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div> --}}
+    <div class="section section-dark text-center">
+      <div class="container">
+        <h2 class="title">Our Team</h2>
+        <div class="row">
+          <div class="col-md-4">
+            <div class="card card-profile card-plain">
+              <div class="card-avatar">
+                <a href="#avatar">
+                  <img src="assets/img/faces/clem-onojeghuo-3.jpg" alt="...">
+                </a>
+              </div>
+              <div class="card-body">
+                <a href="#paper-kit">
+                  <div class="author">
+                    <h4 class="card-title">Muhammad Rosyid</h4>
+                    <h6 class="card-category">Product Manager</h6>
+                  </div>
+                </a>
+              </div>
+              <div class="card-footer text-center">
+                <a href="#pablo" class="btn btn-link btn-just-icon btn-neutral"><i class="fa fa-twitter"></i></a>
+                <a href="#pablo" class="btn btn-link btn-just-icon btn-neutral"><i class="fa fa-google-plus"></i></a>
+                <a href="#pablo" class="btn btn-link btn-just-icon btn-neutral"><i class="fa fa-linkedin"></i></a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="card card-profile card-plain">
+              <div class="card-avatar">
+                <a href="#avatar">
+                  <img src="assets/img/faces/joe-gardner-2.jpg" alt="...">
+                </a>
+              </div>
+              <div class="card-body">
+                <a href="#paper-kit">
+                  <div class="author">
+                    <h4 class="card-title">Anak Agung</h4>
+                    <h6 class="card-category">Designer</h6>
+                  </div>
+                </a>
+              </div>
+              <div class="card-footer text-center">
+                <a href="#pablo" class="btn btn-link btn-just-icon btn-neutral"><i class="fa fa-twitter"></i></a>
+                <a href="#pablo" class="btn btn-link btn-just-icon btn-neutral"><i class="fa fa-google-plus"></i></a>
+                <a href="#pablo" class="btn btn-link btn-just-icon btn-neutral"><i class="fa fa-linkedin"></i></a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="card card-profile card-plain">
+              <div class="card-avatar">
+                <a href="#avatar">
+                  <img src="assets/img/faces/erik-lucatero-2.jpg" alt="...">
+                </a>
+              </div>
+              <div class="card-body">
+                <a href="#paper-kit">
+                  <div class="author">
+                    <h4 class="card-title">Muhammad Arif</h4>
+                    <h6 class="card-category">Developer</h6>
+                  </div>
+                </a>
+              </div>
+              <div class="card-footer text-center">
+                <a href="#pablo" class="btn btn-link btn-just-icon btn-neutral"><i class="fa fa-twitter"></i></a>
+                <a href="#pablo" class="btn btn-link btn-just-icon btn-neutral"><i class="fa fa-google-plus"></i></a>
+                <a href="#pablo" class="btn btn-link btn-just-icon btn-neutral"><i class="fa fa-linkedin"></i></a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="card card-profile card-plain">
+              <div class="card-avatar">
+                <a href="#avatar">
+                  <img src="assets/img/faces/erik-lucatero-2.jpg" alt="...">
+                </a>
+              </div>
+              <div class="card-body">
+                <a href="#paper-kit">
+                  <div class="author">
+                    <h4 class="card-title">Belqis Ivana</h4>
+                    <h6 class="card-category">Developer</h6>
+                  </div>
+                </a>
+              </div>
+              <div class="card-footer text-center">
+                <a href="#pablo" class="btn btn-link btn-just-icon btn-neutral"><i class="fa fa-twitter"></i></a>
+                <a href="#pablo" class="btn btn-link btn-just-icon btn-neutral"><i class="fa fa-google-plus"></i></a>
+                <a href="#pablo" class="btn btn-link btn-just-icon btn-neutral"><i class="fa fa-linkedin"></i></a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="card card-profile card-plain">
+              <div class="card-avatar">
+                <a href="#avatar">
+                  <img src="assets/img/faces/erik-lucatero-2.jpg" alt="...">
+                </a>
+              </div>
+              <div class="card-body">
+                <a href="#paper-kit">
+                  <div class="author">
+                    <h4 class="card-title">Dewi Sulistyowati</h4>
+                    <h6 class="card-category">Developer</h6>
+                  </div>
+                </a>
+              </div>
+              <div class="card-footer text-center">
+                <a href="#pablo" class="btn btn-link btn-just-icon btn-neutral"><i class="fa fa-twitter"></i></a>
+                <a href="#pablo" class="btn btn-link btn-just-icon btn-neutral"><i class="fa fa-google-plus"></i></a>
+                <a href="#pablo" class="btn btn-link btn-just-icon btn-neutral"><i class="fa fa-linkedin"></i></a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="container d-flex justify-content-center align-items-center">
+        <div class="credits">
+          <span class="copyright" style="color:white;">
+            ©
+            <script>
+              document.write(new Date().getFullYear())
+            </script>, made with <i class="fa fa-heart heart"></i> by Group 4 SIB 2D
+          </span>
+        </div>
+      </div>
+    </div>
+    {{-- <div class="section landing-section">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 ml-auto mr-auto">
+            <h2 class="text-center">Keep in touch?</h2>
+            <form class="contact-form">
+              <div class="row">
+                <div class="col-md-6">
+                  <label>Name</label>
+                  <div class="input-group">
+                    <div class="input-group-prepend">
+                      <span class="input-group-text">
+                        <i class="nc-icon nc-single-02"></i>
+                      </span>
+                    </div>
+                    <input type="text" class="form-control" placeholder="Name">
+                  </div>
+                </div>
+                <div class="col-md-6">
+                  <label>Email</label>
+                  <div class="input-group">
+                    <div class="input-group-prepend">
+                      <span class="input-group-text">
+                        <i class="nc-icon nc-email-85"></i>
+                      </span>
+                    </div>
+                    <input type="text" class="form-control" placeholder="Email">
+                  </div>
+                </div>
+              </div>
+              <label>Message</label>
+              <textarea class="form-control" rows="4" placeholder="Tell us your thoughts and feelings..."></textarea>
+              <div class="row">
+                <div class="col-md-4 ml-auto mr-auto">
+                  <button class="btn btn-danger btn-lg btn-fill">Send Message</button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <footer class="footer   footer-white ">
+    <div class="container">
+      <div class="row"> --}}
+        {{-- <nav class="footer-nav">
+          <ul>
+            <li>
+              <a href="https://www.creative-tim.com" target="_blank">Creative Tim</a>
+            </li>
+            <li>
+              <a href="http://blog.creative-tim.com/" target="_blank">Blog</a>
+            </li>
+            <li>
+              <a href="https://www.creative-tim.com/license" target="_blank">Licenses</a>
+            </li>
+          </ul>
+        </nav> --}}
+        
+      {{-- </div>
+    </div> --}}
+  </footer>
+  <!--   Core JS Files   -->
+  <script src="assets/js/core/jquery.min.js" type="text/javascript"></script>
+  <script src="assets/js/core/popper.min.js" type="text/javascript"></script>
+  <script src="assets/js/core/bootstrap.min.js" type="text/javascript"></script>
+  <!--  Plugin for Switches, full documentation here: http://www.jque.re/plugins/version3/bootstrap.switch/ -->
+  <script src="assets/js/plugins/bootstrap-switch.js"></script>
+  <!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
+  <script src="assets/js/plugins/nouislider.min.js" type="text/javascript"></script>
+  <!--  Plugin for the DatePicker, full documentation here: https://github.com/uxsolutions/bootstrap-datepicker -->
+  <script src="assets/js/plugins/moment.min.js"></script>
+  <script src="assets/js/plugins/bootstrap-datepicker.js" type="text/javascript"></script>
+  <!-- Control Center for Paper Kit: parallax effects, scripts for the example pages etc -->
+  <script src="assets/js/paper-kit.js?v=2.2.0" type="text/javascript"></script>
+  <!--  Google Maps Plugin    -->
+  <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
+
+</body>
+
+</html>
